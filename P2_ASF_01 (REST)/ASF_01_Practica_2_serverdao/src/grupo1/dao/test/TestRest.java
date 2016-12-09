28100679997ef68c946d02147e4033c922e72fa6
Aitor Brazaola <aitor.brazaola@opendeusto.es>
2016-12-09 14:29:16 +0100

    Finished CRUD REST API methods for the three entities and created a Test class full featured to test all methods.

--- P2_ASF_01 (REST)/ASF_01_Practica_2_serverdao/src/grupo1/dao/test/TestRest.java
+++ P2_ASF_01 (REST)/ASF_01_Practica_2_serverdao/src/grupo1/dao/test/TestRest.java
@@ -22,6 +22,7 @@ public class TestRest {
 
 		try{
 			// Categories --------------
+			System.out.println("=== CATEGORIES ===");
 			listCategories(service);
 			
 			// Insert new cat
@@ -32,25 +33,81 @@ public class TestRest {
 			
 			listCategories(service);
 			
-			// Delete recently created
+			// Delete category
 			//service.path("rest").path("categories").path("delete").path("74").accept(MediaType.APPLICATION_XML).delete();
 			
 			listCategories(service);
 			
 			Category c = service.path("rest").path("categories").path("31").get(Category.class);
-			System.out.println("Category recovered: " + c.getDescription());
+			System.out.println("Category recovered: " + c.toString());
 			
 			c.setDescription("New description via REST");
 			
 			service.path("rest").path("categories").path("update").path(String.valueOf(c.getId())).put(c);
 			
 			listCategories(service);
+			
+			// Users --------------
+			System.out.println("=== USERS ===");
+			listUsers(service);
+			
+			// Insert new user
+			User newUser = new User();
+			newUser.setName("REST User");
+			newUser.setEmail("rest@isbetter.com");
+			newUser.setPassword("s3cret");
+			service.path("rest").path("users").path("create").accept(MediaType.APPLICATION_XML).post(newUser);
+			
+			listUsers(service);
+			
+			// Delete user
+			//service.path("rest").path("users").path("delete").path("48").accept(MediaType.APPLICATION_XML).delete();
+			
+			listUsers(service);
+			
+			User u = service.path("rest").path("users").path("49").get(User.class);
+			System.out.println("User recovered: " + u.toString());
+			
+			u.setName("New name via REST");
+			
+			service.path("rest").path("users").path("update").path(String.valueOf(u.getId())).put(u);
+			
+			listUsers(service);
+			
+			// Advertisements --------------
+			System.out.println("=== ADS ===");
+			listAds(service);
+			
+			// Insert new user
+			Advertisement newAd = new Advertisement();
+			newAd.setAuthor(u);
+			newAd.setCategory(c);
+			newAd.setDescription("Ad created via rest");
+			newAd.setName("Rest advertisement");
+			service.path("rest").path("advertisements").path("create").accept(MediaType.APPLICATION_XML).post(newAd);
+			
+			listAds(service);
+			
+			// Delete ad
+			//service.path("rest").path("advertisements").path("delete").path("34").accept(MediaType.APPLICATION_XML).delete();
+			
+			listAds(service);
+			
+			Advertisement a = service.path("rest").path("advertisements").path("35").get(Advertisement.class);
+			System.out.println("Ad recovered: " + a.toString());
+			
+			a.setName("New name via REST");
+			
+			service.path("rest").path("advertisements").path("update").path(String.valueOf(a.getId())).put(a);
+			
+			listAds(service);
 		}catch(UniformInterfaceException e){ // if code != 20x exception is thrown
 			ClientResponse r = e.getResponse();
-			System.out.println("categories.GET('application/xml').status: " + r.getStatus());
-			System.out.println("categories.GET('application/xml').entity: " + r.getEntity(String.class));
+			System.out.println("users.GET('application/xml').status: " + r.getStatus());
+			System.out.println("users.GET('application/xml').entity: " + r.getEntity(String.class));
 		}
 	}
+	
 	public static void listCategories(WebResource service) {
 		Category[] categories;
 		categories = service.path("rest")
@@ -58,8 +115,29 @@ public class TestRest {
 				.accept(MediaType.APPLICATION_XML)
 				.get(Category[].class);
 		System.out.println("categories.GET('application/xml').results (con un ARRAY): ");
-		for(int i=0;i<categories.length;i++) System.out.println(categories[i].getId() + "\t" + categories[i].getDescription());
+		for(int i=0;i<categories.length;i++) System.out.println(categories[i].toString());
 	}
+	
+	public static void listUsers(WebResource service) {
+		User[] users;
+		users = service.path("rest")
+				.path("users")
+				.accept(MediaType.APPLICATION_XML)
+				.get(User[].class);
+		System.out.println("users.GET('application/xml').results (con un ARRAY): ");
+		for(int i=0;i<users.length;i++) System.out.println(users[i].toString());
+	}
+	
+	public static void listAds(WebResource service) {
+		Advertisement[] ads;
+		ads = service.path("rest")
+				.path("advertisements")
+				.accept(MediaType.APPLICATION_XML)
+				.get(Advertisement[].class);
+		System.out.println("advertisements.GET('application/xml').results (con un ARRAY): ");
+		for(int i=0;i<ads.length;i++) System.out.println(ads[i].toString());
+	}
+	
 	private static URI getBaseURI() {
 		return UriBuilder.fromUri(
 				"http://localhost:8080/ASF_01_P2").build();
--- P2_ASF_01 (REST)/ASF_01_Practica_2_serverdao/src/resources/UsersResource.java
+++ P2_ASF_01 (REST)/ASF_01_Practica_2_serverdao/src/resources/UsersResource.java
@@ -1,5 +1,75 @@
 package resources;
 
-public class UsersResource {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import grupo1.model.*;
+import grupo1.dao.GestorBD;
+
+@Path("/users")
+public class UsersResource {
+	@GET
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public List<User> getUsers() {
+		ArrayList<User> users = null;
+		try {
+			users = GestorBD.getInstance().getUsers();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return users;
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public User getUser(@PathParam("id") int id) {
+		try {
+			return GestorBD.getInstance().getUser(id);
+		} catch (ClassNotFoundException | SQLException e) {
+			return null;
+		}
+	}
+	
+	@POST
+    @Path("/create")
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Response addUser(User u) {
+		try {
+			GestorBD.getInstance().insertUser(u);
+			return Response.ok().build();
+		} catch (ClassNotFoundException | SQLException e) {
+			return Response.serverError().build();
+		}
+	}
+	
+	@PUT
+    @Path("/update/{id}")
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Response updateUser(@PathParam("id") int id, User u) {
+		try {
+			GestorBD.getInstance().updateUser(u);
+			return Response.ok().build();
+		} catch (ClassNotFoundException | SQLException e) {
+			return Response.serverError().build();
+		}
+	}
+	
+	@DELETE
+    @Path("/delete/{id}")
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Response deleteUser(@PathParam("id") int id) {
+		User userToRemove;
+		try {
+			userToRemove = GestorBD.getInstance().getUser(id);
+			GestorBD.getInstance().deleteUser(userToRemove);
+			return Response.ok().build();
+		} catch (ClassNotFoundException | SQLException e) {
+			return Response.serverError().build();
+		}
+	}
 }
